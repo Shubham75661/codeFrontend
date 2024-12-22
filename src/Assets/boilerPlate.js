cc03df1eae8720069a47f11d33df1f6835f837a9
Shubham <shubham75661@gmail.com>
2024-12-22 23:40:03 +0530

    #ADDED docker file

--- src/Assets/boilerPlate.js
+++ src/Assets/boilerPlate.js
@@ -1,4 +1,4 @@
 export const boilerPlateCode = {
-    javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
+    javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Tony Stark");\n`,
     cpp: `\n#include<iostream>\n#include<bits/stdc++.h>\nusing namespace std;\n\nint main(){\n\treturn 0;\n}\n`
 }
